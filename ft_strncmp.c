e8c823392852441fa827f2586f59784cd767ab51
Miguel angel Fernandez carrillo <migferna@c1r10s1.42madrid.com>
2019-11-09 08:46:39 +0100

    fix ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,26 +6,25 @@
 /*   By: migferna <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/05 15:36:00 by migferna          #+#    #+#             */
-/*   Updated: 2019/11/06 15:08:59 by migferna         ###   ########.fr       */
+/*   Updated: 2019/11/09 08:46:10 by migferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	strncmp(const char *s1, const char *s2, size_t n)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	unsigned char	*ps1;
 	unsigned char	*ps2;
-	unsigned int	i;
 
-	i = 0;
 	ps1 = (unsigned char *)s1;
 	ps2 = (unsigned char *)s2;
-	while (i < n && (s1[i] != '\0' || s2[i] != '\0'))
+	if (n == 0)
+		return (0);
+	while ((--n > 1) && (*ps1 == *ps2) && (*s1 && *s2))
 	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
-		i++;
+		ps1++;
+		ps2++;
 	}
-	return (0);
+	return (*ps1 - *ps2);
 }
