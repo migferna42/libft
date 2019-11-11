0edd3792163ef0b21d9e3633a5c796b21674d9b0
Miguel angel Fernandez carrillo <migferna@c3r14s1.42madrid.com>
2019-11-11 10:16:04 +0100

    fix ft_calloc.c

--- srcs/ft_calloc.c
+++ srcs/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: migferna <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/06 15:23:00 by migferna          #+#    #+#             */
-/*   Updated: 2019/11/10 10:50:09 by migferna         ###   ########.fr       */
+/*   Updated: 2019/11/11 10:11:02 by migferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 void	*ft_calloc(size_t count, size_t size)
 {
-	unsigned char *v;
+	void *v;
 
-	if (!(v = (unsigned char *)malloc(count * size)))
-		return (NULL);
-	ft_bzero(v, '\0');
+	v = malloc(count * size);
+	if (v != NULL && count * size > 0)
+		ft_bzero(v, count * size);
 	return (v);
 }
