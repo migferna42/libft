6cb3865bcb8aa9eb8b9b8a93026cf16279e03999
Miguel angel Fernandez carrillo <migferna@c1r10s1.42madrid.com>
2019-11-11 08:01:41 +0100

    ft_putnbr_fd

--- srcs/ft_putnbr_fd.c
+++ srcs/ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: migferna <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/11 07:38:46 by migferna          #+#    #+#             */
-/*   Updated: 2019/11/11 07:40:53 by migferna         ###   ########.fr       */
+/*   Updated: 2019/11/11 08:01:00 by migferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,20 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
+	unsigned int	nbr;
 
+	if (n < 0)
+	{
+		nbr = (unsigned int)(n * -1);
+		ft_putchar_fd('-', fd);
+	}
+	else
+		nbr = (unsigned int)n;
+	if (nbr < 10)
+		ft_putchar_fd(n + '0', fd);
+	else
+	{
+		ft_putnbr_fd(nbr / 10, fd);
+		ft_putnbr_fd(nbr % 10, fd);
+	}
 }
