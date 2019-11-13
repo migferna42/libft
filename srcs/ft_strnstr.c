374877315b7710369cf6084f5892992e0356b35b
Miguel angel Fernandez carrillo <migferna@c1r10s1.42madrid.com>
2019-11-13 20:07:12 +0100

    Fix ft_strnstr.c

--- srcs/ft_strnstr.c
+++ srcs/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: migferna <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/05 15:47:05 by migferna          #+#    #+#             */
-/*   Updated: 2019/11/13 17:48:29 by migferna         ###   ########.fr       */
+/*   Updated: 2019/11/13 20:06:02 by migferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,18 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	int i;
 	int j;
 
-	if (!needle[0])
-		return ((char *)haystack);
 	i = 0;
-	while (haystack != '\0' && i < (int)len)
+	while (haystack[i] != '\0' && i < (int)len)
 	{
 		j = 0;
 		while (needle[j] != '\0' && haystack[i + j] == needle[j] && (i + j) < (int)len)
-		{
-			if (needle[j + 1] == '\0')
-				return ((char *)&haystack[i]);
 			j++;
-		}
+		if (!needle[j])
+			return ((char *)&haystack[i]);
+		j = 0;
 		i++;
 	}
-	return (0);
+	if (!needle[0])
+		return ((char *)haystack);
+	return (NULL);
 }
