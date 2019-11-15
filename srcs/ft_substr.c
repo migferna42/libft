8449fc1737937f473403b0354fab960d79c2d1c7
Miguel angel Fernandez carrillo <migferna@c1r10s1.42madrid.com>
2019-11-15 09:55:30 +0100

    Fix ft_substr.c

--- srcs/ft_substr.c
+++ srcs/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: migferna <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/09 09:24:28 by migferna          #+#    #+#             */
-/*   Updated: 2019/11/10 08:24:12 by migferna         ###   ########.fr       */
+/*   Updated: 2019/11/15 09:53:30 by migferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	i = 0;
 	if (!s || !(substr = malloc(sizeof(char ) * len + 1)))
 		return (NULL);
-	while (len--)
+	while (start < ft_strlen(s) && len--)
 	{
 		substr[i] = s[i + start];
 		i++;
