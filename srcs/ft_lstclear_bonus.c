b4c95b8fcb5eb933a9a9143a6c7cc50cc9715c73
Miguel angel Fernandez carrillo <migferna@c1r10s1.42madrid.com>
2019-11-15 15:35:15 +0100

    ft_lstclear.c

--- srcs/ft_lstclear_bonus.c
+++ srcs/ft_lstclear_bonus.c
@@ -6,7 +6,7 @@
 /*   By: migferna <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/15 11:40:27 by migferna          #+#    #+#             */
-/*   Updated: 2019/11/15 11:49:18 by migferna         ###   ########.fr       */
+/*   Updated: 2019/11/15 12:47:11 by migferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,14 @@
 
 void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
+	t_list *ptr;
 
+	ptr = *lst;
+	while (ptr->next)
+	{
+		del(ptr->content);
+		ptr = ptr->next;
+		free(lst);
+	}
+	ptr = NULL;
 }
