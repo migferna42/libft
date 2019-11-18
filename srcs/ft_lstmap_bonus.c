ad54f779343d6c3dd96dbc0f33bdb89b1311c67b
Miguel angel Fernandez carrillo <migferna@c1r10s1.42madrid.com>
2019-11-18 08:33:38 +0100

    ft_lstmap.c

--- srcs/ft_lstmap_bonus.c
+++ srcs/ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: migferna <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/16 12:50:24 by migferna          #+#    #+#             */
-/*   Updated: 2019/11/16 17:12:10 by migferna         ###   ########.fr       */
+/*   Updated: 2019/11/18 08:32:49 by migferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,22 @@
 t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {	
 	t_list *head;
-	t_list *it;
+	t_list *list;
 
 	if (!lst)
 		return (NULL);
-	it = f(lst->content);
-	head = it;
+	list = ft_lstnew(f(lst->content));
+	head = list;
 	while (lst->next)
 	{
 		lst = lst->next;
-		it->content = f(lst->content);
-		it = it->next;
+		if (!(list->next = ft_lstnew(f(lst->content))))
+		{
+			del(list->next);
+			free(list->next);
+			return (NULL);
+		}
+		list = list->next;
 	}
 	return (head);
 }
