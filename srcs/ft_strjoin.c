a11256affaa8900316e7273dee14ad67e2afe427
Miguel angel Fernandez carrillo <migferna@c2r11s4.42madrid.com>
2019-11-19 08:50:27 +0100

    Fix ft_strjoin.c

--- srcs/ft_strjoin.c
+++ srcs/ft_strjoin.c
@@ -6,55 +6,36 @@
 /*   By: migferna <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/11 07:42:28 by migferna          #+#    #+#             */
-/*   Updated: 2019/11/15 09:32:53 by migferna         ###   ########.fr       */
+/*   Updated: 2019/11/19 08:49:53 by migferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char		*ft_strcpy(char *dst, const char *src)
-{
-	int it;
-
-	it = 0;
-	while (src[it] != '\0')
-	{
-		dst[it] = src[it];
-		it++;
-	}
-	dst[it] = '\0';
-	return (dst);
-}
-
-static char		*ft_strcat(char *s1, const char *s2)
-{
-	int i;
-	int j;
-
-	i = 0;
-	while (s1[i] != '\0')
-		i++;
-	j = 0;
-	while (s2[j] != '\0')
-	{
-		s1[i + j] = s2[j];
-		j++;
-	}
-	s1[i + j] = '\0';
-	return (s1);
-}
-
-char			*ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char *s3;
-
-	s3 = NULL;
-	if (s1 && s2)
+	int it;
+	int it2;
+	
+	if (!s1 || !s2)
+		return (NULL);
+	it = 0;
+	it2 = 0;
+	s3 = malloc(sizeof(char) * ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!s3)
+		return (NULL);
+	while (s1[it] != '\0')
 	{
-		if (!(s3 = malloc(sizeof(char) * ft_strlen(s1) + ft_strlen(s2))))
-			return (NULL);
-		ft_strcpy(s3, s1);
-		ft_strcat(s3, s2);
+		s3[it] = s1[it];
+		it++;
 	}
+	while (s2[it2] != '\0')
+	{
+		s3[it] = s2[it2];
+		it++;
+		it2++;
+	}
+	s3[it] = '\0';
 	return (s3);
 }
